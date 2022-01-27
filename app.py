a724d17719c0aafbda2ab9ec8d3ebf1b420797c2
James Downs <jdowns06@gmail.com>
2022-01-27 12:49:31 -0600

    modify

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
